44a54721c0b89944668ec137cfed0ecdcf6fe590
prisha thapar <prisha.thapar@gmail.com>
2024-11-24 22:19:19 +0530

    us16

--- app/etc/config.php
+++ app/etc/config.php
@@ -359,6 +359,7 @@ return [
         'Prisha_Mod1' => 1,
         'Prisha_Mod14' => 1,
         'Prisha_Mod15' => 1,
+        'Prisha_Mod16' => 1,
         'Prisha_Mod2' => 1,
         'Prisha_Mod3' => 1,
         'Prisha_Mod4' => 1,
