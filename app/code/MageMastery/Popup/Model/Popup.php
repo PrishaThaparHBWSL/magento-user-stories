af53e50368221ee0f68fed2c190960278d2dcda0
prisha thapar <prisha.thapar@gmail.com>
2024-11-26 12:22:06 +0530

    working on us22

---
+++ app/code/MageMastery/Popup/Model/Popup.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace MageMastery\Popup\Model;
+
+use MageMastery\Popup\Model\ResourceModel\Popup as PopupResource;
+use \Magento\Framework\Model\AbstractModel;
+
+class Popup extends AbstractModel implements PopupInterface
+{
+    protected function _construct()
+    {
+        $this->_init(PopupResource::class);
+    }
+}
--- app/etc/config.php
+++ app/etc/config.php
@@ -354,6 +354,7 @@ return [
         'Klarna_Kp' => 1,
         'Klarna_Onsitemessaging' => 1,
         'Klarna_KpGraphQl' => 1,
+        'MageMastery_Popup' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Prisha_Mod1' => 1,
